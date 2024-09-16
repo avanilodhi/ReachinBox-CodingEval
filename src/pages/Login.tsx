b1956cfe10e8464bd1a1195fbdfb4760bf179c6b
avanilodhi <avanilodhi29@gmail.com>
2024-09-16 21:59:51 +0530

    login update

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -13,7 +13,7 @@ function Login() {
 
   const handleGoogleLogin = () => {
     // Redirect to Google login URL
-    window.location.href = "https://hiring.reachinbox.xyz/api/v1/auth/google-login?redirect_to=https://reach-inbox-ai-three.vercel.app/";
+    window.location.href = "https://hiring.reachinbox.xyz/api/v1/auth/google-login?redirect_to=https://reachinbox-codingeval.vercel.app";
   };
 
   return (
